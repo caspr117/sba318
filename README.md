5ba5192913177febe00652ac6ed9ccfde4bc8153
caspr117 <caspr117@gmail.com>
2025-06-15 22:16:04 -0400

    Last commit?

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sba318
+this app Creates , gets , deletes Users , has middleware and is spread across multiple files and renders ejs on one of the requests I believe.
--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,14 @@
 // import{addUser,obj} from "./data.js"
 // import {axios} from "./axios"
-let button = document.getElementById("button")
+let button = document.getElementById("button");
 
+button.addEventListener("click", async () => {
+  document.querySelectorAll("p").forEach((p) => p.remove());
 
-
-button.addEventListener("click",async()=>{
-let r = await axios.get("http://localhost:5000/posts")
-r.data.forEach(element =>{
-    let p = document.createElement("p")
-    p.textContent = JSON.stringify(element)
-    document.body.appendChild(p)
-})
-
-
-// console.log(r.data[0])
-})
+  let r = await axios.get("http://localhost:5000/posts");
+  r.data.forEach((element) => {
+    let p = document.createElement("p");
+    p.textContent = JSON.stringify(element);
+    document.body.appendChild(p);
+  });
+});
